f8afa291fbe356d700adcf223ec231783adecc68
GabrielaZamcu <gabriela.zamcu@gmail.com>
2024-09-09 13:26:13 +0300

    New Commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,424 @@
-import { TextField } from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import axios from "axios";
+import Rating from "react-rating-stars-component";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Grid,
+  Avatar,
+  ClickAwayListener,
+  Grow,
+  Paper,
+  Popper,
+  MenuItem,
+  MenuList,
+  Stack,
+} from "@mui/material";
+import { useNavigate } from "react-router";
 
-export default function Home() {
-  return (
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+const defaultCenter = {
+  lat: 47.67014946856104,
+  lng: 26.2874623814687,
+};
+
+const currentMarkerIcon = {
+  url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
+};
+
+function Home() {
+  const navigate = useNavigate();
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCo_yoy3nhE2Fbb5GEOr_KB7DbowQTaOVU",
+  });
+
+  const [locations, setLocations] = useState([]);
+  const [role, setRole] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [userRating, setUserRating] = useState(0);
+  const [imageUrl, setImageUrl] = useState("");
+  const [initials, setInitials] = useState("");
+  const [newMarker, setNewMarker] = useState({
+    name: "",
+    lat: 0,
+    lng: 0,
+    description: "",
+    imageUrl: "",
+  });
+
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  useEffect(() => {
+    fetchData();
+    getCurrentLocation();
+    const userRole = localStorage.getItem("role");
+    setRole(userRole);
+    setInitials(getInitials());
+  }, []);
+  useEffect(() => {
+    const user = localStorage.getItem("userId");
+    if (!user) {
+      navigate("/Login");
+    } else {
+    }
+  }, [open]);
+
+  const getInitials = () => {
+    const name = localStorage.getItem("username");
+    const nameArray = name.split(" ");
+    const initials = nameArray.map((word) => word.charAt(0)).join("");
+    return initials.toUpperCase();
+  };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/marker");
+      setLocations(response.data);
+    } catch (error) {
+      console.error("Error fetching locations:", error);
+    }
+  };
+
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation({
+            lat: latitude,
+            lng: longitude,
+          });
+        },
+        (error) => {
+          console.error("Error getting current location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  const handleMarkerClick = (marker) => {
+    setSelectedLocation(marker);
+    setUserRating(marker.rating || 0);
+    setImageUrl(marker.imageUrl || "/logo.png");
+  };
+
+  const handleInfoWindowClose = () => {
+    setSelectedLocation(null);
+  };
+
+  const handleRatingChange = async (newRating) => {
+    setUserRating(newRating);
+    const updatedLocation = { ...selectedLocation, rating: newRating };
+    setSelectedLocation(updatedLocation);
+
+    try {
+      await axios.post(`/marker/${updatedLocation._id}/rate`, {
+        rating: newRating,
+      });
+      fetchData();
+    } catch (error) {
+      console.error("Error updating rating:", error);
+    }
+  };
+
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      const imageUrl = response.data.url;
+      setImageUrl(imageUrl);
+
+      if (selectedLocation) {
+        await axios.post(`/marker/${selectedLocation._id}/update`, {
+          imageUrl,
+        });
+        fetchData();
+      } else {
+        setNewMarker({ ...newMarker, imageUrl });
+      }
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  };
+
+  const handleNewMarkerSubmit = async () => {
+    try {
+      const response = await axios.post("/marker", newMarker);
+      if (response.status === 200 || response.status === 201) {
+        setLocations([...locations, response.data]);
+        setNewMarker({
+          name: "",
+          lat: 0,
+          lng: 0,
+          description: "",
+          imageUrl: "",
+        });
+      } else {
+        console.error("Error adding new marker");
+      }
+    } catch (error) {
+      console.error("Error adding new marker", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewMarker({ ...newMarker, [name]: value });
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    localStorage.clear();
+    setOpen(false);
+  };
+
+  const handleListKeyDown = (event) => {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (open) {
+      anchorRef.current.focus();
+    }
+  }, [open]);
+
+  return isLoaded ? (
     <div>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <Stack
+        direction="row"
+        spacing={2}
+        style={{ position: "absolute", top: "10px", right: "10px", zIndex: 1 }}
+      >
+        <div>
+          <Avatar
+            ref={anchorRef}
+            id="composition-avatar"
+            aria-controls={open ? "composition-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+            style={{ cursor: "pointer" }}
+          >
+            {initials}
+          </Avatar>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            placement="bottom-start"
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === "bottom-start" ? "left top" : "left bottom",
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="composition-menu"
+                      aria-labelledby="composition-avatar"
+                      onKeyDown={handleListKeyDown}
+                    >
+                      <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+      </Stack>
+
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={currentLocation || defaultCenter}
+        zoom={12}
+      >
+        {currentLocation && (
+          <Marker
+            position={currentLocation}
+            icon={currentMarkerIcon}
+            onClick={() =>
+              handleMarkerClick({
+                lat: currentLocation.lat,
+                lng: currentLocation.lng,
+                name: "Locația ta curentă",
+                imageUrl: process.env.PUBLIC_URL + "/logo.png",
+              })
+            }
+          />
+        )}
+
+        {locations.map((location) => (
+          <Marker
+            key={location._id}
+            position={{ lat: location.lat, lng: location.lng }}
+            onClick={() => handleMarkerClick(location)}
+          />
+        ))}
+
+        {selectedLocation?.description ? (
+          <InfoWindow
+            position={{ lat: selectedLocation.lat, lng: selectedLocation.lng }}
+            onCloseClick={handleInfoWindowClose}
+          >
+            <Card sx={{ maxWidth: 345 }}>
+              {imageUrl && (
+                <CardMedia
+                  component="img"
+                  alt={selectedLocation.name}
+                  height="200"
+                  image={imageUrl}
+                  title={selectedLocation.name}
+                />
+              )}
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {selectedLocation.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {selectedLocation.description}
+                </Typography>
+                <Box component="fieldset" borderColor="transparent">
+                  <Grid container alignItems="center">
+                    <Grid item>
+                      <Rating
+                        count={5}
+                        value={userRating}
+                        onChange={handleRatingChange}
+                        size={24}
+                        activeColor="#ffd700"
+                      />
+                    </Grid>
+                    <Grid item>
+                      <Typography
+                        component="span"
+                        style={{ marginLeft: "8px" }}
+                      >
+                        {userRating}/5
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </CardContent>
+            </Card>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+
+      {role === "admin" ? (
+        <div
+          style={{ position: "absolute", top: "10px", left: "10px", zIndex: 1 }}
+        >
+          <Card sx={{ maxWidth: 345 }}>
+            <CardContent>
+              <Typography variant="h6">Adaugă un nou marker</Typography>
+              <div>
+                <label>
+                  Nume:
+                  <input
+                    type="text"
+                    name="name"
+                    value={newMarker.name}
+                    onChange={handleChange}
+                    style={{ width: "100%", marginBottom: "10px" }}
+                  />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Latitudine:
+                  <input
+                    type="number"
+                    name="lat"
+                    value={newMarker.lat}
+                    onChange={handleChange}
+                    style={{ width: "100%", marginBottom: "10px" }}
+                  />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Longitudine:
+                  <input
+                    type="number"
+                    name="lng"
+                    value={newMarker.lng}
+                    onChange={handleChange}
+                    style={{ width: "100%", marginBottom: "10px" }}
+                  />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Descriere:
+                  <input
+                    type="text"
+                    name="description"
+                    value={newMarker.description}
+                    onChange={handleChange}
+                    style={{ width: "100%", marginBottom: "10px" }}
+                  />
+                </label>
+              </div>
+              <input type="file" onChange={handleImageUpload} />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNewMarkerSubmit}
+              >
+                Adaugă Marker
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      ) : null}
     </div>
+  ) : (
+    <></>
   );
 }
+
+export default React.memo(Home);
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,40 +1,177 @@
-import { TextField } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Stack,
+  Switch,
+  FormControlLabel,
+  Typography,
+} from "@mui/material";
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  function onLogin() {
-    navigate("/Home");
+  async function handleFormSubmit() {
+    setErrorMessage("");
+    if (isSignUp) {
+      try {
+        const response = await axios.post("/users", {
+          username: username,
+          password: password,
+          email: email,
+          role: role,
+        });
+
+        if (response.status === 201) {
+          console.log("Utilizator creat:", response.data);
+          navigate("/Home", { state: { role: response.data.role } });
+        } else {
+          setErrorMessage(
+            "Eroare la crearea utilizatorului. Vă rugăm să încercați din nou."
+          );
+        }
+      } catch (error) {
+        console.error("Eroare la crearea utilizatorului", error);
+        setErrorMessage(
+          "Eroare la crearea utilizatorului. Vă rugăm să încercați din nou."
+        );
+      }
+    } else {
+      try {
+        const response = await axios.post("/users/login", {
+          email: email,
+          password: password,
+        });
+
+        if (
+          (response.status === 200 || response.status === 201) &&
+          response.data._id
+        ) {
+          console.log("Utilizator autentificat:", response.data);
+          localStorage.setItem("userId", response.data._id);
+          localStorage.setItem("username", response.data.username);
+          localStorage.setItem("password", response.data.password);
+          localStorage.setItem("email", response.data.email);
+          localStorage.setItem("role", response.data.role);
+
+          navigate("/Home", { state: { role: response.data.role } });
+        } else {
+          setErrorMessage("Utilizator indisponibil sau parolă incorectă.");
+        }
+      } catch (error) {
+        console.error("Eroare la autentificare", error);
+        setErrorMessage(
+          "Eroare la autentificare. Vă rugăm să încercați din nou."
+        );
+      }
+    }
   }
 
   return (
-    <div
-      style={{ position: "relative", display: "grid", justifyItems: "center" }}
-    >
+    <div style={{ position: "relative", height: "100vh", overflow: "hidden" }}>
       <img
         src={"Logo1.png"}
         alt="Descriere imagine"
-        style={{ width: "80%", height: "auto" }}
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: -1,
+        }}
       />
       <div
         style={{
           position: "absolute",
-          top: "50%",
-          transform: "translateY(-50%)",
+          top: "60%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
           display: "grid",
-          gap: "8px",
+          gap: "16px",
           justifyItems: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          padding: "32px",
+          borderRadius: "8px",
         }}
       >
-        <TextField id="outlined-name" label="Name" variant="outlined" />
-        <TextField id="outlined-surname" label="Surname" variant="outlined" />
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isSignUp}
+              onChange={() => {
+                setIsSignUp(!isSignUp);
+                setErrorMessage(""); // Șterge mesajul de eroare la schimbare
+              }}
+            />
+          }
+          label={
+            isSignUp ? "Comutare la Autentificare" : "Comutare la Înregistrare"
+          }
+        />
+        {isSignUp ? (
+          <>
+            <TextField
+              id="outlined-username"
+              label="Nume utilizator"
+              variant="outlined"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              id="outlined-password"
+              label="Parolă"
+              variant="outlined"
+              type="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <TextField
+              id="outlined-email"
+              label="Email"
+              variant="outlined"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              id="outlined-role"
+              label="Rol"
+              variant="outlined"
+              required
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </>
+        ) : (
+          <>
+            <TextField
+              id="outlined-username"
+              label="Email"
+              variant="outlined"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              id="outlined-password"
+              label="Parolă"
+              variant="outlined"
+              type="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </>
+        )}
+        {errorMessage && <Typography color="error">{errorMessage}</Typography>}
         <Stack direction="row" spacing={2}>
-          <Button variant="contained" onClick={onLogin}>
-            Login
+          <Button variant="contained" onClick={handleFormSubmit}>
+            {isSignUp ? "Înregistrare" : "Autentificare"}
           </Button>
         </Stack>
       </div>
